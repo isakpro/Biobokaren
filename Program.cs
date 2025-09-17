9a4b35da1b683cdb9751ec01a469f6f150d4abc7
isakr <isakr@DESKTOP-QENPR6F>
2025-09-17 13:56:06 +0200

    added cases 1-4 in menu for movie list, select movie/time/tickets, student discount and receipt

--- Program.cs
+++ Program.cs
@@ -39,8 +39,60 @@ namespace Biobokaren
                 }
 
                 switch (menuChoice)
+
                 {
-        }
+                    case 1:
+                        ListMovies(movies, showTimes, basePrices);
+                        break;
+
+                    case 2:
+                        ListMovies(movies, showTimes, basePrices);
+                        Console.Write("Select a movie by number: ", movies.Length - 1);
+                        if ( (!int.TryParse(Console.ReadLine(), out int movieIdx) || movieIdx < 0 || movieIdx >= movies.Length)
+                        {
+                            Console.WriteLine("Invalid movie selection.");
+                            break;
+                        }
+
+                        selectedMovie = movieIdx;
+
+                        Console.WriteLine("Available showtimes:");
+                        for (int i = 0; i < showTimes.Length; i++)
+                            Console.WriteLine($"{i}. {showTimes[i]}");
+                        Console.WriteLine("Enter the index of the desired showtime: ");
+                        if (!int.TryParse(Console.ReadLine(), out int timeIdx) || timeIdx < 0 || timeIdx >= showTimes.Length)
+                        {
+                            Console.WriteLine("Invalid showtime selection.");
+                            selectedTimeIndex = -1;
+                            break;
+                        }
+                        selectedTimeIndex = timeIdx;
+
+                        Console.Write("Enter number of tickets: ");
+                        if (!int.TryParse(Console.ReadLine(), out ticketCount) || ticketCount <= 0)
+                        {
+                            Console.WriteLine("Invalid ticket count.");
+                            ticketCount = 0;
+                            break;
+                        }
+                        ticketCount = tickets;
+
+                        Console.WriteLine($"Selectedt: {movies[selectedMovie]} at {showTimes[selectedTimeIndex]} for {ticketCount} tickets.");
+                        break;
+
+                    case 3:
+                        isStudent = !isStudent;
+                        Console.WriteLine(isStudent ? "Student discount applied." : "Student discount removed.");
+                        break;
+
+                    case 4:
+                        if (selectedMovie < 0 || selectedTimeIndex < 0 || ticketCountt <= 0)
+                            {
+                            Console.WriteLine("Select a movie, showtime, and ticket count first.");
+                            break;
+                        }
+
+
         
     }
 }
