af60b740c8ec6bbf7fde8ac0520eca2ebdcb2989
isakr <isakr@DESKTOP-QENPR6F>
2025-09-17 13:37:34 +0200

    Added a main menu loop with user input and variables

--- Program.cs
+++ Program.cs
@@ -20,6 +20,26 @@ namespace Biobokaren
             string[] movies = { "The Shawshank Redemption", "The Dark Knight", "Forrest Gump" };
             string[] showTimes = { "12:00", "15:00", "19:00" };
             double[] prices = { 100.0, 120.0, 150.0 };
+
+            int selectedMovie = -1;
+            int selectedTimeIndes = -1;
+            int ticketCount = 0;
+            bool isStudent = false;
+
+            bool running = true;
+            while (running)
+
+                {
+
+                ShowMenu();
+                if (!int.TryParse(Console.ReadLine(), out int menuChoice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    continue;
+                }
+
+                switch (menuChoice)
+                {
         }
         
     }
